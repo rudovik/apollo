6063f9b3ec961d95d37f3937a74c58e2a89f2221
rudovik <flashwrite@ukr.net>
2024-03-13 11:32:35 +0200

    Changes

--- app/api/graphql/route.ts
+++ app/api/graphql/route.ts
@@ -3,6 +3,7 @@ import connectToMongoDB from "lib/mongooseSingleton"
 import { NextRequest } from "next/server"
 
 async function handler(req: NextRequest) {
+  console.log(process.env.NEXT_PUBLIC_VERCEL_URL || "hello")
   const _handler = await createApolloServer()
   await connectToMongoDB()
 
