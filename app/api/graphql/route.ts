c24ccd12079fbca80ffdb043ff97f24a37d01d02
rudovik <flashwrite@ukr.net>
2024-03-13 10:19:58 +0200

    Changes

--- app/api/graphql/route.ts
+++ app/api/graphql/route.ts
@@ -42,7 +42,7 @@ async function handler(req: NextRequest) {
 
   const schema = await buildSchema({
     resolvers: [WoodResolver],
-    emitSchemaFile: true,
+    emitSchemaFile: false,
     scalarsMap: [{ type: ObjectId, scalar: ObjectIdScalar }],
     validate: true,
   })
