1bc97d38a24e30753eaa18da7bf5cb0545bef0b5
rudovik <flashwrite@ukr.net>
2024-03-13 17:23:52 +0200

    Changes

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,48 @@
 "use client"
-
-// import { useSuspenseQuery } from "@apollo/experimental-nextjs-app-support/ssr"
-import { useGetAllWoodsSuspenseQuery } from "lib/graphql/GetAllWoods.graphql"
+import { HomeSlider } from "components/HomeSlider"
+import { HomePromotion } from "components/HomePromotion"
+import { useGetSortedProductsSuspenseQuery } from "lib/graphql/GetSortedProducts.graphql"
+import { SortBy } from "__generated__/__types__"
+import { CardBlock } from "components/CardBlock"
 
 export default function Page() {
-  // const { data } = useSuspenseQuery<any>(GetAllWoodsDocument, {
-  //   errorPolicy: "ignore",
-  // })
+  const {
+    data: bySoldData,
+    // loading: bySoldLoading,
+    error: bySoldError,
+    client,
+  } = useGetSortedProductsSuspenseQuery({
+    variables: {
+      sortBy: SortBy.Sold,
+      limit: 4,
+      order: "desc",
+    },
+  })
 
-  const { data } = useGetAllWoodsSuspenseQuery()
-  // console.log(data)
-  return <h1>Hello, Next.js</h1>
+  const {
+    data: byArrivalData,
+    // loading: byArrivalLoading,
+    error: byArrivalError,
+  } = useGetSortedProductsSuspenseQuery({
+    variables: {
+      sortBy: SortBy.CreatedAt,
+      limit: 4,
+      order: "desc",
+    },
+  })
+
+  return (
+    <div>
+      <HomeSlider />
+
+      <CardBlock
+        list={bySoldData.getSortedProducts}
+        title="Best Selling Guitars"
+      />
+
+      <HomePromotion />
+
+      <CardBlock list={byArrivalData.getSortedProducts} title="New Arrivals" />
+    </div>
+  )
 }
