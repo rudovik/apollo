97f66f96d62b9ca2c1cdc258dbbe171b261d1f2e
rudovik <flashwrite@ukr.net>
2024-03-13 17:37:25 +0200

    changes

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,14 +5,31 @@ import { ApolloWrapper } from "lib/apolloWrapper"
 import { getHostAndCookie } from "lib/getHostAndCookie"
 import { AuthProvider } from "lib/useAuth"
 
+import { oswald } from "./fonts"
+import "./global.css"
+import { Header } from "components/Header"
+import { Footer } from "components/Footer"
+
 export default async function RootLayout({ children }) {
   const { host, cookie } = getHostAndCookie()
   return (
     <html lang="en">
+      <head>
+        <meta charSet="utf-8" />
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1, shrink-to-fit=no"
+        />
+        <meta name="theme-color" content="#000000" />
+      </head>
       <AppRouterCacheProvider>
         <ApolloWrapper host={host} cookies={cookie}>
           <AuthProvider cookie={cookie}>
-            <body>{children}</body>
+            <body className={oswald.className}>
+              <Header />
+              <div className="page_container">{children}</div>
+              <Footer />
+            </body>
           </AuthProvider>
         </ApolloWrapper>
       </AppRouterCacheProvider>
