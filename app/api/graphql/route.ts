fc3487676e9f270c72c796f15714d8e11ca7ea2e
rudovik <flashwrite@ukr.net>
2024-03-13 09:49:46 +0200

    Changes

--- app/api/graphql/route.ts
+++ app/api/graphql/route.ts
@@ -1,11 +1,33 @@
-import initServer from "lib/apollo-singleton"
+// import initServer from "lib/apollo-singleton"
 import { NextRequest } from "next/server"
+import { ApolloServer } from "@apollo/server"
+import { startServerAndCreateNextHandler } from "@as-integrations/next"
+import { gql } from "graphql-tag"
 
-async function handler(req: NextRequest) {
-  const _handler = await initServer()
+// async function handler(req: NextRequest) {
+//   const _handler = await initServer()
 
-  const response = await _handler(req)
-  return response
+//   const response = await _handler(req)
+//   return response
+// }
+
+const resolvers = {
+  Query: {
+    hello: () => "world",
+  },
 }
+
+const typeDefs = gql`
+  type Query {
+    hello: String
+  }
+`
+
+const server = new ApolloServer({
+  resolvers,
+  typeDefs,
+})
+
+const handler = startServerAndCreateNextHandler(server)
 
 export { handler as GET, handler as POST }
