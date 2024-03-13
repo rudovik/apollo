6798a013a69fc724d2979876171e5c38b1fb2118
rudovik <flashwrite@ukr.net>
2024-03-13 12:35:10 +0200

    Changes

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,13 @@
 "use client"
 
-import { useGetAllWoodsSuspenseQuery } from "lib/graphql/GetAllWoods.graphql"
+// import { useGetAllWoodsSuspenseQuery } from "lib/graphql/GetAllWoods.graphql"
+import { useSuspenseQuery } from "@apollo/experimental-nextjs-app-support/ssr"
+import { GetAllWoodsDocument } from "lib/graphql/GetAllWoods.graphql"
 
 export default function Page() {
-  const {
-    data: { getAllWoods: woods },
-    error: woodsError,
-  } = useGetAllWoodsSuspenseQuery()
-  console.log(woods)
+  const { data } = useSuspenseQuery<any>(GetAllWoodsDocument, {
+    errorPolicy: "ignore",
+  })
+  console.log(data)
   return <h1>Hello, Next.js</h1>
 }
